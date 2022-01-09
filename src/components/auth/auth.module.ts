0098791e201a5d162a9eca97e0a136212a17c3a2
sirkenedy <owoputikehide@gmail.com>
2022-01-10 00:40:08 +0100

    [IMP] finish up the authentication components

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategy/local.strategy';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { jwtConstants } from './constants';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule, 
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '24h' },
+    }),
+
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
