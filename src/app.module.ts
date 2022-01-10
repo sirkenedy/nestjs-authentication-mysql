f8c3db130301e3033583f3c2039ca7b6451232f5
sirkenedy <owoputikehide@gmail.com>
2022-01-10 01:05:05 +0100

    random update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// book module imported
 import { BooksModule } from './components/books/books.module';
 import { AuthModule } from './components/auth/auth.module';
 import { UsersModule } from './components/users/users.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -19,6 +20,9 @@ import { UsersModule } from './components/users/users.module';
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_INTERCEPTOR,
+    useClass: AuthInterceptor,
+  }],
 })
 export class AppModule {}
